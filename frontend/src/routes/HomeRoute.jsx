96ada2df5a78298148aaca2fe21f1d957845ba6c
Gregory Dechant <dechantg@gmail.com>
2023-11-07 15:16:57 -0800

    added cyoa option to filter by liked results. the filter is handled by clicking the like button in the topnavbar

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PhotoList from '../components/PhotoList';
 import TopNavigation from '../components/TopNavigationBar';
 import useHomeRouteHook from '../hooks/homeRouteHook';
@@ -16,14 +16,21 @@ const HomeRoute = (props) => {
     likeResults,
   } = useHomeRouteHook(props.photos, props.likeDataModal, props.onTopicSelect, props.clickForModal);
 
+  const [favIconClicked, setFavIconClicked] = useState(false);
+
+
   console.log("is liked from homeroute", isLiked);
   console.log("likeresults from inside the homeroute but i am serious", likeResults);
   // console.log("like status/data from home route", likeData);
 
   const handleFavIconClick = () => {
     console.log("FavIcon button was clicked");
-    // You can add more logic here if needed
+    console.log("photos object", props.photos);
+    setFavIconClicked(!favIconClicked);
   };
+
+  const filteredPhotos = favIconClicked ? props.photos.filter((photo) => photo.id in likeResults) : props.photos;
+
 
   return (
     <div className="home-route">
@@ -33,7 +40,12 @@ const HomeRoute = (props) => {
         onTopicSelect={handleTopicSelection}
         onFavIconClick={handleFavIconClick}
       />
-      <PhotoList photos={props.photos} likeStatus={likeData} onClick={handleArticleClick} />
+
+      <PhotoList
+        photos={filteredPhotos}
+        likeStatus={likeData}
+        onClick={handleArticleClick}
+      />
     </div>
   );
 };
